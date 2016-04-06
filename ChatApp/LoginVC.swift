3d824d8e10cedeb766ead8d24d6d3e9024267a8c
Vi Zi <vizillion@gmail.com>
2016-04-06 20:15:02 +0700

    signup View Controller

--- ChatApp/LoginVC.swift
+++ ChatApp/LoginVC.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var signupBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let theWidth = view.frame.size.width
+        let theHeight = view.frame.size.height
+        
+        welcomeLabel.center = CGPointMake(theWidth/2, 130)
+        usernameTxt.frame = CGRectMake(16, 200, theWidth-32, 30)
+        passwordTxt.frame = CGRectMake(16, 240, theWidth-32, 30)
+        loginBtn.center = CGPointMake(theWidth/2, 330)
+        signupBtn.center = CGPointMake(theWidth/2, theHeight-30)
     }
 
     override func didReceiveMemoryWarning() {
--- ChatApp/SignUpVC.swift
+++ ChatApp/SignUpVC.swift
@@ -7,13 +7,73 @@
 //
 
 import UIKit
+import Parse
 
-class SignUpVC: UIViewController {
+class SignUpVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var addImgBtn: UIButton!
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var profileNameTxt: UITextField!
+    @IBOutlet weak var signupBtn: UIButton!
+    
+    // MARK: - Action
+    
+    @IBAction func addImgBtn_click(sender: UIButton) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = true
+        self.presentViewController(image, animated: true, completion: nil)
+    }
+    
+    @IBAction func signupBtn_click(sender: UIButton) {
+        
+        let user = PFUser()
+        user.username = usernameTxt.text
+        user.password = passwordTxt.text
+        user.email = usernameTxt.text
+        user["profileName"] = profileNameTxt.text
+        
+        let imageData = UIImagePNGRepresentation(self.profileImg.image!)
+        let imageFile = PFFile(name: "profilePhoto.png", data: imageData!)
+        user["photo"] = imageFile
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, signUpError: NSError?) -> Void in
+            
+            if signUpError == nil {
+                
+                print("signup")
+            } else {
+                
+                print("can't signup")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameTxt.delegate = self
+        passwordTxt.delegate = self
+        profileNameTxt.delegate = self
 
-        // Do any additional setup after loading the view.
+        let theWidth = view.frame.size.width
+//        let theHeight = view.frame.size.height
+        
+        profileImg.center = CGPointMake(theWidth/2, 140)
+        profileImg.layer.cornerRadius = profileImg.frame.size.width/2
+        profileImg.clipsToBounds = true
+        
+        addImgBtn.center = CGPointMake(self.profileImg.frame.maxX+50, 140)
+        usernameTxt.frame = CGRectMake(16, 230, theWidth-32, 30)
+        passwordTxt.frame = CGRectMake(16, 270, theWidth-32, 30)
+        profileNameTxt.frame = CGRectMake(16, 310, theWidth-32, 30)
+        signupBtn.center = CGPointMake(theWidth/2, 380)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +81,61 @@ class SignUpVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: -Image
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        profileImg.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
-    */
+}
 
+extension SignUpVC: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        usernameTxt.resignFirstResponder()
+        passwordTxt.resignFirstResponder()
+        profileNameTxt.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+        let theWidth = view.frame.size.width
+        let theHeight = view.frame.size.height
+        
+        if(UIScreen.mainScreen().bounds.height == 568) {
+            if(textField == self.profileNameTxt) {
+                
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                    
+                    self.view.center = CGPointMake(theWidth/2, (theHeight/2)-40)
+                    }, completion: { (finished: Bool) -> Void in
+                        
+                        //
+                })
+            }
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+        let theWidth = view.frame.size.width
+        let theHeight = view.frame.size.height
+        
+        if(UIScreen.mainScreen().bounds.height == 568) {
+            if(textField == self.profileNameTxt) {
+                
+                UIView.animateWithDuration(0.3, delay: 0, options: .CurveLinear, animations: { () -> Void in
+                    
+                    self.view.center = CGPointMake(theWidth/2, theHeight/2)
+                    }, completion: { (finished: Bool) -> Void in
+                        
+                        //
+                })
+            }
+        }
+    }
 }
